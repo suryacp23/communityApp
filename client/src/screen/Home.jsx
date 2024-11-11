0d6d5c95b1289ac46ae3848ae1b90950bc685152
Ravi1470 <vk8580318@gmail.com>
2024-11-11 21:39:11 +0530

    button style added

--- client/src/screen/Home.jsx
+++ client/src/screen/Home.jsx
@@ -3,9 +3,13 @@ import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <div>
-      <nav>
-        <Link to="/signup">Sign Up</Link>
-        <Link to="/login">Log In</Link>
+      <nav className=" flex-col text-right m-3 space-x-1 text-slate-100">
+        <Link className="p-2 rounded-lg bg-gray-600 w-4" to="/signup">
+          Sign Up
+        </Link>
+        <Link className=" p-2 pr-3 pl-3 rounded-lg bg-gray-600 " to="/login">
+          Log In
+        </Link>
       </nav>
     </div>
   );
