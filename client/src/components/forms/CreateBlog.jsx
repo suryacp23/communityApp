80f60230551e296c8dabbd39a5a186a97cac17bd
suryacp23 <suryacp.com@gmail.com>
2024-11-14 21:31:46 +0530

    basic forms added

---
+++ client/src/components/forms/CreateBlog.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const CreateBlog = () => {
+  return (
+    <div>
+      <form action="">
+        <input type="text" />
+        <input type="text" />
+        <input type="file" />
+        <button>create blog</button>
+      </form>
+    </div>
+  );
+};
+
+export default CreateBlog;
---
+++ client/src/components/forms/EditBlog.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const EditBlog = () => {
+  return (
+    <div>
+      <form action="">
+        <input type="text" />
+        <input type="text" />
+        <input type="file" />
+        <button>save blog</button>
+      </form>
+    </div>
+  );
+};
+
+export default EditBlog;
