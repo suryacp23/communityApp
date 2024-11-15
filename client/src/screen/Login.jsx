8c7a4996daa4244e689a05ef34937b253c82f8d6
Ravi1470 <vk8580318@gmail.com>
2024-11-15 11:04:48 +0530

    login style added

--- client/src/screen/Login.jsx
+++ client/src/screen/Login.jsx
@@ -14,35 +14,35 @@ const Login = () => {
   };
   return (
     <div className="flex h-[100vh] w-full  justify-center items-center">
-      <div
-        className=" text-white flex flex-col h-96 w-72 shadow-gray-700
-       hover:shadow-gray-500 shadow-md items-center "
-      >
-        <h4 className="m-4">LOGIN</h4>
-        <form action="" className=" flex flex-col items-center">
+      <div className=" text-white flex flex-col h-96 w-72 bg-black rounded-md items-center ">
+        <div className="h-1/6">
+          <h4 className="m-4">LOGIN</h4>
+        </div>
+        <form
+          action=""
+          className=" flex flex-col w-full h-1/2 items-center justify-around ">
           <input
-            className=" pl-2 w-52 h-8 mt-8 rounded-sm  bg-gray-600"
+            className=" pl-2 w-52 h-8 rounded-sm  bg-gray-600"
             type="text"
             placeholder="Username"
             onChange={(e) => setUsername(e.target.value)}
             required
           />
           <input
-            className="pl-2 mt-8 h-8 w-52 rounded-sm bg-gray-600 "
+            className="pl-2 h-8 w-52 rounded-sm bg-gray-600 "
             type="password"
             placeholder="Password"
             onChange={(e) => setPassword(e.target.value)}
             required
           />
           <button
-            className="bg-blue-500 shadow-lg  hover:shadow-blue-500/40  w-20 h-7 m-8 flex justify-center rounded-3xl"
-            onClick={handleLogin}
-          >
+            className="bg-blue-500 shadow-lg  hover:shadow-blue-500/40  w-52 h-8  flex justify-center rounded-sm"
+            onClick={handleLogin}>
             {loading ? "loading" : "log in"}
           </button>
         </form>
-        <div className="flex ">
-          <p className="text-sm m-1 ">Don't have an account?</p>
+        <div className="flex w-full items-center justify-center h-1/6 pt-5 ">
+          <p className="text-sm ">Don't have an account?</p>
           <Link className="text-blue-400" to={`/signup`}>
             Sign up
           </Link>
