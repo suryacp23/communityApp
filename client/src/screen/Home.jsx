c8e4545812b211be588ae57b17094efa82d666b7
Ravi1470 <vk8580318@gmail.com>
2024-11-17 23:12:58 +0530

    rebase done

--- client/src/screen/Home.jsx
+++ client/src/screen/Home.jsx
@@ -8,11 +8,9 @@ import {
   MenuTrigger,
 } from "../components/ui/menu";
 import { Avatar } from "../components/ui/avatar";
-
 import logo from "../assets/logo.svg";
 import { useBlog } from "../hooks/useBlog";
 import Blog from "../components/blog/Blog";
-
 const Demo = () => {
   return <Avatar name="Segun Adebayo" src="https://bit.ly/sage-adebayo" />;
 };
@@ -91,47 +89,6 @@ const Home = () => {
           return <Blog key={blog._id} blog={blog} />;
         })}
       </div>
-<<<<<<< HEAD
-      <nav className=" flex justify-end m-3 space-x-2 text-slate-100">
-        {user ? (
-          <MenuRoot>
-            <MenuTrigger asChild>
-              <div>
-                <Avatar name={user.userName} size="2xl"  className="cursor-pointer bg-[]" />
-              </div>
-            </MenuTrigger>
-            <MenuContent>
-              <MenuItem value="useremail">{user.userName}</MenuItem>
-              <MenuItem value="useremail">{user.email}</MenuItem>
-              <MenuItem
-                value="signout"
-                color="fg.error"
-                _hover={{ bg: "bg.error", color: "fg.error" }}
-                onClick={handleSignout}
-              >
-                Sign out
-              </MenuItem>
-            </MenuContent>
-          </MenuRoot>
-        ) : (
-          <>
-            <Link
-              className="flex justify-center items-center p-2 w-20 rounded-md bg-blue-800 hover:bg-blue-600 "
-              to="/signup"
-            >
-              Sign Up
-            </Link>
-            <Link
-              className=" flex justify-center items-center p-2 w-20 rounded-md bg-blue-800 hover:bg-blue-600 "
-              to="/login"
-            >
-              Log In
-            </Link>
-          </>
-        )}
-      </nav>
-=======
->>>>>>> cc81c84 (blogs are added)
     </div>
   );
 };
