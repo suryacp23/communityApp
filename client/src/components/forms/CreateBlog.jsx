9900c79db8540e68e07a53f3f6595383ded0d01e
suryacp23 <suryacp.com@gmail.com>
2024-11-20 23:04:55 +0530

    rebased with ui

--- client/src/components/forms/CreateBlog.jsx
+++ client/src/components/forms/CreateBlog.jsx
@@ -1,13 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
+import { useBlog } from "../../hooks/useBlog";
+import { useAuth } from "../../hooks/useAuth";
 
 const CreateBlog = () => {
+  const { createBlog } = useBlog();
+  const { user } = useAuth();
+  const [formData, setFormData] = useState({
+    title: "",
+    description: "",
+    imageUrl: "",
+    user: user._id,
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    createBlog(formData);
+  };
+
   return (
     <div>
-      <form action="">
-        <input type="text" />
-        <input type="text" />
-        <input type="file" />
-        <button>create blog</button>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="title" onChange={handleChange} />
+        <textarea
+          type="text"
+          name="description"
+          value={formData.description}
+          onChange={handleChange}
+        />
+        <input
+          type="file"
+          name="imageUrl"
+          value={formData.file}
+          onChange={handleChange}
+        />
+        <button type="submit">create blog</button>
       </form>
     </div>
   );
