4c8a85c7d715811158ee1dfef9b095d5fe47380f
Ravi1470 <vk8580318@gmail.com>
2024-11-21 17:14:16 +0530

    bug fixed
    header design
    
    home page updated
    
    buckfix
    
    bug fix

--- client/src/screen/Home.jsx
+++ client/src/screen/Home.jsx
@@ -1,93 +1,24 @@
 import { useEffect } from "react";
-import { Link } from "react-router-dom";
-import { useAuth } from "../hooks/useAuth";
-import {
-  MenuContent,
-  MenuItem,
-  MenuRoot,
-  MenuTrigger,
-} from "../components/ui/menu";
-import { Avatar } from "../components/ui/avatar";
-import logo from "../assets/logo.svg";
 import { useBlog } from "../hooks/useBlog";
 import Blog from "../components/blog/Blog";
-const Demo = () => {
-  return <Avatar name="Segun Adebayo" src="https://bit.ly/sage-adebayo" />;
-};
+import Header from "../components/bigcomponents/Header";
 
 const Home = () => {
-  const { user, login, logout, loading } = useAuth();
   const { blogs, getBlogs } = useBlog();
   useEffect(() => {
     getBlogs();
   }, []);
   console.log(blogs);
-  const handleSignout = async () => {
-    await logout();
-  };
+
   return (
-    <div className="p-2">
-      <div className="h-[10vh] bg-transparent flex justify-between items-center ">
-        <div className="">
-          <img src={logo} alt="logo" className="h-10 w-10 text-black" />
+    <div className="overflow-hidden select-none text-background bg-richblack">
+      <Header />
+      <div className="flex items-center justify-around m-14">
+        <div className=" grid grid-cols-3 grid-rows-* gap-x-16">
+          {blogs?.map((blog) => {
+            return <Blog key={blog._id} blog={blog} />;
+          })}
         </div>
-        <nav className=" flex justify-end m-3 space-x-2 text-slate-100">
-          <ul className="flex justify-center items-center gap-2">
-            {user ? (
-              <>
-                <li>
-                  <Link
-                    to={"/createPost"}
-                    className="flex justify-center items-center p-2 rounded-md bg-blue-800 hover:bg-blue-600"
-                  >
-                    create post
-                  </Link>
-                </li>
-                <li>
-                  <MenuRoot>
-                    <MenuTrigger asChild>
-                      <div>
-                        <Avatar name={user.userName} size="2xl" />
-                      </div>
-                    </MenuTrigger>
-                    <MenuContent>
-                      <MenuItem value="useremail">{user.userName}</MenuItem>
-                      <MenuItem value="useremail">{user.email}</MenuItem>
-                      <MenuItem
-                        value="signout"
-                        color="fg.error"
-                        _hover={{ bg: "bg.error", color: "fg.error" }}
-                        onClick={handleSignout}
-                      >
-                        Sign out
-                      </MenuItem>
-                    </MenuContent>
-                  </MenuRoot>
-                </li>
-              </>
-            ) : (
-              <>
-                <Link
-                  className="flex justify-center items-center p-2 w-20 rounded-md bg-blue-800 hover:bg-blue-600 "
-                  to="/signup"
-                >
-                  Sign Up
-                </Link>
-                <Link
-                  className=" flex justify-center items-center p-2 w-20 rounded-md bg-blue-800 hover:bg-blue-600 "
-                  to="/login"
-                >
-                  Log In
-                </Link>
-              </>
-            )}
-          </ul>
-        </nav>
-      </div>
-      <div>
-        {blogs?.map((blog) => {
-          return <Blog key={blog._id} blog={blog} />;
-        })}
       </div>
     </div>
   );
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#ecf0f1",
+        secondary: "#34495e",
+        background: "#7f8c8d",
+        accent: "#1abc9c",
+        richblack: "#0D0C1D",
+        oxford_blue: "#161B33",
+        ultra_violet: "#474973",
+        lavender_bush: "#F6E8EA",
+        robin_blue: "#06BCC1",
+        new_blue: "#152138",
+      },
+    },
   },
   plugins: [],
 };
