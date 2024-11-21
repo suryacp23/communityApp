9ab8fb77934f27eee3796f7b8e246d0522f3d9d7
SRI-HARIPRIYAN <sris.haripriyan@gmail.com>
2024-11-21 21:29:02 +0530

    added middleware and updated blog schema

--- server/routes/blogRoutes.js
+++ server/routes/blogRoutes.js
@@ -1,17 +1,19 @@
 import express from "express";
 import {
-  createBlog,
-  updateBlog,
-  deleteBlog,
-  getblogs,
-  getBlogById,
+	createBlog,
+	updateBlog,
+	deleteBlog,
+	getblogs,
+	getBlogById,
 } from "../controllers/blogController.js";
+
+import protect from "../middleware/protect.js";
 
 const router = express.Router();
 
 router.route("/").post(createBlog);
-router.route("/:id").put(updateBlog);
-router.route("/:id").delete(deleteBlog);
-router.route("/blogs").get(getblogs);
+router.route("/:id").put(protect, updateBlog);
+router.route("/:id").delete(protect, deleteBlog);
+router.route("/blog").get(getblogs);
 router.route("/:blogid").get(getBlogById);
 export default router;
