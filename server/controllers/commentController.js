46ed60a7a9ca480b66f597f7f1a2768d6ae0bc19
suryacp23 <suryacp.com@gmail.com>
2024-11-22 12:55:39 +0530

    comment feature added in backend

---
+++ server/controllers/commentController.js
@@ -0,0 +1,27 @@
+import Comment from "../models/commentModel.js";
+export const createComment = async (req, res, next) => {
+  try {
+    const { comment, blog, user } = req.body;
+    const newComment = new Comment({
+      comment,
+      blog,
+      user,
+    });
+    await newComment.save();
+    res.status(200).json(newComment);
+  } catch (error) {
+    console.log("createComment controller error" + error.message);
+    res.status(400).json({ error: error.message });
+  }
+};
+export const getPostComments = async (req, res, next) => {
+  try {
+    const comments = await Comment.find({ blogId: req.params.blogId }).sort({
+      createdAt: -1,
+    });
+    res.status(200).json(comments);
+  } catch (error) {
+    console.log("getpostComment controller error" + error.message);
+    res.status(400).json({ error: error.message });
+  }
+};
