46b79fe9f3f24a18680df3b3c261e83c515112fd
SRI-HARIPRIYAN <sris.haripriyan@gmail.com>
2024-11-26 23:09:54 +0530

    Myblog edit and delete added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,27 +8,33 @@ import CreateBlog from "./components/forms/CreateBlog";
 import EditBlog from "./components/forms/EditBlog";
 import Blogpage from "./screen/Blogpage";
 import { CommentProvider } from "./hooks/useComments";
+import MyBlogs from "./screen/user/MyBlogs";
 // import logo from "./assets/logo.svg";
 
 const App = () => {
-  return (
-    <div>
-      <BlogProvider>
-        <AuthProvider>
-          <CommentProvider>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/signup" element={<Signup />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/createPost" element={<CreateBlog />} />
-              <Route path="/editBlog" element={<EditBlog />} />
-              <Route path="/blogs" element={<Blogpage />} />
-            </Routes>
-          </CommentProvider>
-        </AuthProvider>
-      </BlogProvider>
-    </div>
-  );
+	return (
+		<div>
+			<BlogProvider>
+				<AuthProvider>
+					<CommentProvider>
+						<Routes>
+							<Route path="/" element={<Home />} />
+							<Route path="/signup" element={<Signup />} />
+							<Route path="/login" element={<Login />} />
+							<Route
+								path="/createPost"
+								element={<CreateBlog />}
+							/>
+							<Route path="/editBlog" element={<EditBlog />} />
+							<Route path="/blogs" element={<Blogpage />} />
+							<Route path="/myBlogs" element={<MyBlogs />} />
+							<Route path="*" element={<Home />} />
+						</Routes>
+					</CommentProvider>
+				</AuthProvider>
+			</BlogProvider>
+		</div>
+	);
 };
 
 export default App;
--- client/src/hooks/useAuth.jsx
+++ client/src/hooks/useAuth.jsx
@@ -5,123 +5,129 @@ import { toast } from "react-toastify";
 
 const AuthContext = createContext();
 export const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [status, setStatus] = useState(null);
-  const navigate = useNavigate();
+	const [user, setUser] = useState(
+		localStorage.getItem("token")
+			? JSON.parse(localStorage.getItem("token"))
+			: null
+	);
+	const [loading, setLoading] = useState(false);
+	const [status, setStatus] = useState(null);
+	const navigate = useNavigate();
 
-  const login = async (userName, password) => {
-    try {
-      setLoading(true);
-      const response = await fetch(`/api/auth/login`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ userName, password }),
-      });
-      if (!response.ok) {
-        const errorData = await response.json();
-        setStatus(errorData.error);
-        toast(errorData.error);
-      }
+	const login = async (userName, password) => {
+		try {
+			setLoading(true);
+			const response = await fetch(`/api/auth/login`, {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({ userName, password }),
+			});
+			if (!response.ok) {
+				const errorData = await response.json();
+				setStatus(errorData.error);
+				toast(errorData.error);
+			}
 
-      const fetchedUser = await response.json();
-      setUser(fetchedUser);
-      localStorage.setItem("token", JSON.stringify(fetchedUser));
-      if (response.ok) {
-        setStatus("login successfully");
-        navigate("/");
-      }
-    } catch (error) {
-      console.log(error);
-      setStatus("login failed");
-    } finally {
-      setLoading(false);
-    }
-  };
+			const fetchedUser = await response.json();
+			setUser(fetchedUser);
+			localStorage.setItem("token", JSON.stringify(fetchedUser));
+			if (response.ok) {
+				setStatus("login successfully");
+				navigate("/");
+			}
+		} catch (error) {
+			console.log(error);
+			setStatus("login failed");
+		} finally {
+			setLoading(false);
+		}
+	};
 
-  const signup = async (userName, email, password) => {
-    try {
-      setLoading(true);
-      const response = await fetch(`/api/auth/signup`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ userName, email, password }),
-      });
-      if (!response.ok) {
-        const errorData = await response.json();
-        setStatus(errorData.error);
-        toast(errorData.error);
-      }
+	const signup = async (userName, email, password) => {
+		try {
+			setLoading(true);
+			const response = await fetch(`/api/auth/signup`, {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({ userName, email, password }),
+			});
+			if (!response.ok) {
+				const errorData = await response.json();
+				setStatus(errorData.error);
+				toast(errorData.error);
+			}
 
-      const newUser = await response.json();
-      setUser(newUser);
-      localStorage.setItem("token", newUser.token);
-      if (response.ok) {
-        setStatus("signed up successfully");
-        navigate("/");
-      }
-    } catch (error) {
-      setStatus("signup failed");
-    } finally {
-      setLoading(false);
-    }
-  };
+			const newUser = await response.json();
+			setUser(newUser);
+			localStorage.setItem("token", newUser.token);
+			if (response.ok) {
+				setStatus("signed up successfully");
+				navigate("/");
+			}
+		} catch (error) {
+			setStatus("signup failed");
+		} finally {
+			setLoading(false);
+		}
+	};
 
-  const logout = async () => {
-    try {
-      setLoading(true);
-      await fetch(`/api/auth/logout`, {
-        method: "POST",
-        // headers: {
-        //   "Content-Type": "application/json",
-        //   Authorization: `Bearer ${localStorage.getItem("token")}`,
-        // },
-      });
-      setUser(null);
-      localStorage.removeItem("token");
-      setStatus("Signed out successfully");
-      navigate("/");
-    } catch (error) {
-      setStatus(error);
-    } finally {
-      setLoading(false);
-    }
-  };
+	const logout = async () => {
+		try {
+			setLoading(true);
+			await fetch(`/api/auth/logout`, {
+				method: "POST",
+				// headers: {
+				//   "Content-Type": "application/json",
+				//   Authorization: `Bearer ${localStorage.getItem("token")}`,
+				// },
+			});
+			setUser(null);
+			localStorage.removeItem("token");
+			setStatus("Signed out successfully");
+			navigate("/");
+		} catch (error) {
+			setStatus(error);
+		} finally {
+			setLoading(false);
+		}
+	};
 
-  // useEffect(() => {
-  //   const token = localStorage.getItem("token");
-  //   const fetchUser = async () => {
-  //     if (token) {
-  //       try {
-  //         const response = await fetch(`${baseUrl}/me`, {
-  //           headers: {
-  //             Authorization: `Bearer ${token}`,
-  //           },
-  //         });
-  //         if (!response.ok) throw new Error("Invalid token");
+	// useEffect(() => {
+	//   const token = localStorage.getItem("token");
+	//   const fetchUser = async () => {
+	//     if (token) {
+	//       try {
+	//         const response = await fetch(`${baseUrl}/me`, {
+	//           headers: {
+	//             Authorization: `Bearer ${token}`,
+	//           },
+	//         });
+	//         if (!response.ok) throw new Error("Invalid token");
 
-  //         const fetchedUser = await response.json();
-  //         setUser(fetchedUser);
-  //       } catch (error) {
-  //         console.error("Error fetching user:", error);
-  //         setUser(null);
-  //         localStorage.removeItem("token");
-  //       }
-  //     }
-  //     setLoading(false);
-  //   };
-  //   fetchUser();
-  // }, []);
-  console.log(status);
-  const value = { user, loading, status, login, logout, signup };
+	//         const fetchedUser = await response.json();
+	//         setUser(fetchedUser);
+	//       } catch (error) {
+	//         console.error("Error fetching user:", error);
+	//         setUser(null);
+	//         localStorage.removeItem("token");
+	//       }
+	//     }
+	//     setLoading(false);
+	//   };
+	//   fetchUser();
+	// }, []);
 
-  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+	const value = { user, loading, status, login, logout, signup };
+
+	return (
+		<AuthContext.Provider value={value}>{children}</AuthContext.Provider>
+	);
 };
 
 export const useAuth = () => {
-  return useContext(AuthContext);
+	return useContext(AuthContext);
 };
