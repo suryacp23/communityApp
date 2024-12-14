fef8e444c75e7aafa9cbc5a6f282cb6b0bb9b521
Ravi1470 <vk8580318@gmail.com>
2024-12-14 16:32:58 +0530

    eventspage

--- client/src/Version1/Home1.jsx
+++ client/src/Version1/Home1.jsx
@@ -8,15 +8,13 @@ import { useBlog } from "../hooks/useBlog";
 import Blog from "../components/blog/Blog";
 
 const Home1 = () => {
- 
-
   return (
-    <div className=" bg-primary1 relative  selection:bg-blue-200 selection:text-black ">
+    <div className=" bg-primary1  h-screen w-screen select-none flex flex-col justify-center items-center">
       <Navbar />
       <div className="h-3/5 w-12 bg-pink-600 absolute left-0 bottom-0 rounded-tr-full"></div>
       <div className="h-3/5 w-12 bg-blue-500 absolute top-0 right-0 rounded-bl-full"></div>
-      <div className=" h-[88vh]   flex  flex-col justify-around items-center relative">
-        <div className="h-3/5 w-3/5 bg-[#1f1f1f] rounded-3xl flex  justify-center items-center">
+      <div className=" h-[88vh]   flex  flex-col justify-around items-center ">
+        <div className="h-3/5 w-3/5 bg-[#1f1f1f] rounded-3xl flex  justify-center items-center ">
           <h2 className="text-4xl p-10 font-mochiy text-center  capitalize ">
             Simple registration, clear plans, and smooth communication for
             flawless events.
@@ -26,17 +24,10 @@ const Home1 = () => {
           to={"/login1"}
           className="flex justify-around h-11 w-2/6  items-center rounded-full text-blue-800 text-xl bg-black select-none">
           get started
-      
         </Link>
-        
-        
       </div>
     </div>
-    
   );
 };
-        
-     
-        
-    
+
 export default Home1;
