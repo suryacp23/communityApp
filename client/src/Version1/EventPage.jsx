2f80a18d23e550cb7fe488c0a26741b32c61d309
Ravi1470 <vk8580318@gmail.com>
2024-12-14 16:32:58 +0530

    eventspage

--- client/src/Version1/EventPage.jsx
+++ client/src/Version1/EventPage.jsx
@@ -1,28 +1,43 @@
 import React from "react";
 import Navbar from "../components/bigcomponents/Navbar";
-import Blog from "../components/blog/Blog";
+// import Blog from "../components/blog/Blog";
 import SpinnerLogo from "../components/Additionalui/SpinnerLogo";
 import { useQuery } from "@tanstack/react-query";
-
-
-
-// const { isPending } = useQuery({
-//   queryKey:["events"],
-//   queryFn: async () => {
-//     const response = await fetch("/api/blog/blogs");
-//     return await response.json();
-//   },
-// });
+import { fetchBlogs } from "./api";
+import Events from "./user/Events";
+import Spinner from "../components/Additionalui/Spinner";
 
 const EventPage = () => {
+  const { isLoading, error, isError, data } = useQuery({
+    queryKey: ["events"],
+    queryFn: fetchBlogs,
+  });
+  if (isLoading)
+    return (
+      <div className="h-screen w-screen flex justify-center items-center backdrop-blur-3xl ">
+        <Spinner className="" />
+      </div>
+    );
+  if (isError) return <div>{error.message}</div>;
+  console.log(data);
+  const blogs = data?.blogs || [];
   return (
-    <div className="bg-primary1 w-screen h-screen ">
-      <div className="w-10 h-[80vh] bg-pink-600 absolute top-20 left-0 rounded-e-full"></div>
-      <div className="w-10 h-[80vh] bg-blue-500 absolute  right-0 top-20 rounded-s-full"></div>
-      <Navbar />
-      {/* {blogs?.map((blog) => {
-        return <Blog key={blog._id} blog={blog} />;
-      })} */}
+    <div className="bg-primary1 w-screen h-screen flex justify-center relative items-center flex-col select-none">
+      <div className=" absolute top-0">
+      <Navbar className=" " />
+
+      </div>
+      <div className="w-10 h-[84vh] bg-pink-600 absolute top-14 left-0 rounded-e-full"></div>
+      <div className="w-10 h-[84vh] bg-blue-500 absolute  right-0 top-14 rounded-s-full"></div>
+      <div className="w-[100vw] h-[80vh] flex justify-center scroll-smooth overflow-y-scroll">
+        <div className="grid grid-cols-2 grid-row-* h-4/6 w-4/5 gap-10">
+          {blogs.map((blog) => (
+            <div className=" ">
+              <Events key={blog._id} blog={blog} />
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
