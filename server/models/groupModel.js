51ba3cd3be5751f49763b3d0416cf1188fe68b14
surya c <suryacp.com@gmail.com>
2025-01-11 19:29:59 +0530

    updated event model & delete event (#46) (#47)
    Co-authored-by: Sri Haripriyan <115647705+SRI-HARIPRIYAN@users.noreply.github.com>

--- server/models/groupModel.js
+++ server/models/groupModel.js
@@ -1,13 +1,18 @@
 import mongoose from "mongoose";
 
-const groupSchema = mongoose.Schema({
-	name: { type: String, required: true },
-	description: String,
-	eventId: { type: String, required: true },
-	admin: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-	moderators: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }], // Moderators
-	members: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-});
+const groupSchema = mongoose.Schema(
+	{
+		name: { type: String, required: true },
+		description: String,
+		eventId: { type: mongoose.Schema.Types.ObjectId, ref: "Event" },
+		admin: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+		moderators: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }], // Moderators
+		members: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+	},
+	{
+		timestamps: true,
+	}
+);
 
 const Group = mongoose.model("Group", groupSchema);
 
