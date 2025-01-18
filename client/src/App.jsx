8732cf9b69ce909a8dcc73a74aefffd83a57e204
surya c <suryacp.com@gmail.com>
2025-01-18 19:33:07 +0530

    Backend (#49)
    * some changes
    
    * updated event model & delete event (#46) (#47)
    
    Co-authored-by: Sri Haripriyan <115647705+SRI-HARIPRIYAN@users.noreply.github.com>
    
    * Attendance feature (#48)
    
    * updated event model & delete event (#46) (#47)
    
    Co-authored-by: Sri Haripriyan <115647705+SRI-HARIPRIYAN@users.noreply.github.com>
    
    * attendance feature completed
    
    ---------
    
    Co-authored-by: surya c <suryacp.com@gmail.com>
    
    ---------
    
    Co-authored-by: Sri Haripriyan <115647705+SRI-HARIPRIYAN@users.noreply.github.com>

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,44 +7,49 @@ import Login from "./screen/Login";
 import Signup from "./screen/Signup";
 import Group from "./screen/Group";
 import GetStarted from "./screen/GetStarted";
+import Attendancepage from "./screen/Attendancepage";
 
 const navLinks = [
-  { label: "Events", href: "/events" },
-  { label: "Groups", href: "/groups" },
-  { label: "Create Events", href: "/create-events" },
-  { label: "Dashboard", href: "/dashboard" },
+	{ label: "Events", href: "/events" },
+	{ label: "Groups", href: "/groups" },
+	{ label: "Create Events", href: "/create-events" },
+	{ label: "Dashboard", href: "/dashboard" },
 ];
 
 const CreateEvents = () => (
-  <div className="w-full h-screen">
-    <Header navLinks={navLinks} />
-    <section>Create Events Page</section>
-  </div>
+	<div className="w-full h-screen">
+		<Header navLinks={navLinks} />
+		<section>Create Events Page</section>
+	</div>
 );
 const Dashboard = () => (
-  <div className="w-full h-screen">
-    <Header navLinks={navLinks} />
-    <section>Dashboard</section>
-  </div>
+	<div className="w-full h-screen">
+		<Header navLinks={navLinks} />
+		<section>Dashboard</section>
+	</div>
 );
 
 const App = () => {
-  return (
-    <Router>
-      <div className="max-w-screen-wide bg-background mx-auto">
-        <Routes>
-          <Route path="/" element={<GetStarted />} />
-          <Route path="/events" element={<Events />} />
-          <Route path="/events/:eventId" element={<Event />} />
-          <Route path="/groups" element={<Group />} />
-          <Route path="/create-events" element={<CreateEvents />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-        </Routes>
-      </div>
-    </Router>
-  );
+	return (
+		<Router>
+			<div className="max-w-screen-wide bg-background mx-auto">
+				<Routes>
+					<Route path="/" element={<GetStarted />} />
+					<Route path="/events" element={<Events />} />
+					<Route path="/events/:eventId" element={<Event />} />
+					<Route path="/groups" element={<Group />} />
+					<Route path="/create-events" element={<CreateEvents />} />
+					<Route path="/dashboard" element={<Dashboard />} />
+					<Route path="/login" element={<Login />} />
+					<Route path="/signup" element={<Signup />} />
+					<Route
+						path="/events/:eventId/attendance"
+						element={<Attendancepage />}
+					/>
+				</Routes>
+			</div>
+		</Router>
+	);
 };
 
 export default App;
