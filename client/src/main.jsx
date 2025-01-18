766fa5912ff7b45ac83f3d17b990253586f32267
Sri Haripriyan <115647705+SRI-HARIPRIYAN@users.noreply.github.com>
2025-01-18 19:32:35 +0530

    Attendance feature (#48)
    * updated event model & delete event (#46) (#47)
    
    Co-authored-by: Sri Haripriyan <115647705+SRI-HARIPRIYAN@users.noreply.github.com>
    
    * attendance feature completed
    
    ---------
    
    Co-authored-by: surya c <suryacp.com@gmail.com>

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,18 +3,20 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { AuthProvider } from "./hooks/useAuth.jsx";
 import { SocketContextProvider } from "./context/socketContext.jsx";
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <AuthProvider>
-        <SocketContextProvider>
-          <App />
-        </SocketContextProvider>
-      </AuthProvider>
-    </QueryClientProvider>
-  </StrictMode>
+	<StrictMode>
+		<QueryClientProvider client={queryClient}>
+			<AuthProvider>
+				<SocketContextProvider>
+					<App />
+					<ReactQueryDevtools initialIsOpen={false} />
+				</SocketContextProvider>
+			</AuthProvider>
+		</QueryClientProvider>
+	</StrictMode>
 );
--- client/src/screen/Attendancepage.jsx
+++ client/src/screen/Attendancepage.jsx
@@ -1,22 +1,218 @@
-import React from "react";
-import { useQuery } from "@tanstack/react-query";
-import { attendance } from "../services/api";
-import Spinner from "../components/"
+import React, { useRef, useEffect, useState } from "react";
+import { useParams, useSearchParams } from "react-router-dom";
+import { Html5Qrcode } from "html5-qrcode";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import { checkAttendance, getAttendances } from "../services/api";
 
 const Attendancepage = () => {
-	const { isLoading, data } = useQuery({
+	const { eventId } = useParams();
+	const queryClient = useQueryClient();
+	const { data, isLoading, isFetching, isError } = useQuery({
 		queryKey: ["attendance"],
-		queryFn: attendance,
-		refetchInterval: 100,
+		queryFn: () => getAttendances(eventId),
 	});
-	if (isLoading) {
-		return <Spinner/>;
-	}
+	const present =
+		data?.attendances?.filter((el) => el.isAttended === true).length || 0;
+
+	const { mutate, data: scannedUser } = useMutation({
+		mutationKey: ["check-attendance"],
+		mutationFn: checkAttendance,
+		onSuccess: (scannedUser) => {
+			queryClient.setQueryData(["attendance"], (prev) => {
+				if (!prev) return;
+				return {
+					//update the attendance cache
+					...prev,
+					attendances: prev?.attendances.map((el) =>
+						el.email === scannedUser?.data.email
+							? {
+									...el,
+									isAttended: scannedUser?.data.isAttended,
+							  }
+							: el
+					),
+				};
+			});
+		},
+	});
+
+	const html5QrCode = useRef(null);
+	const config = { fps: 20, qrbox: { width: 300, height: 300 } };
+
+	const startScanner = () => {
+		if (html5QrCode.current) {
+			try {
+				console.log("Starting QR code scanner...");
+				html5QrCode.current.start(
+					{ facingMode: "user" },
+					config,
+					(decodedText, decodedResult) => {
+						try {
+							const [decodedEvent, applicationId] =
+								decodedText.split("==");
+							mutate({
+								eventId,
+								decodedText: {
+									eventId: decodedEvent,
+									applicationId,
+								},
+							});
+						} catch (error) {
+							throw error;
+						}
+					}
+				);
+			} catch (err) {
+				console.log("Error starting scanner: " + err.error);
+			}
+		} else {
+			console.log("Scanner is not initiated properly");
+		}
+	};
+
+	const stopScanner = () => {
+		if (html5QrCode.current) {
+			html5QrCode.current
+				.stop()
+				.then(() => {
+					console.log("QR code scanner stopped...");
+				})
+				.catch((err) => {
+					console.log("Error stopping the scanner: " + err.error);
+				});
+		} else {
+			console.warn("Scanner is not running");
+		}
+	};
+	useEffect(() => {
+		html5QrCode.current = new Html5Qrcode("reader");
+	}, []);
+
 	return (
-		<div>
-			{data.map((attend) => {
-				return <p>{attend.name + " " + attend.isAttended}</p>;
-			})}
+		<div className="text-white font-mono max-w-screen-2xl h-screen o">
+			<header className="text-center py-3 text-pink-300 ">
+				<h1 className="font-bold text-lg bg-gray-800 w-fit mx-auto py-2 px-5 rounded-full">
+					Event Title: Google codemonger
+				</h1>
+			</header>
+			<main className="md:p-3">
+				<div>
+					<h2 className=" text-lg py-2">Applicants information:-</h2>
+					<div className="flex gap-5 w-fit bg-primary px-3 py-2">
+						<p>Total: {data?.attendances.length}</p>
+						<p className="text-green-300">Present: {present}</p>
+						<p className="text-red-300">
+							Absent:{data?.attendances?.length - present}
+						</p>
+					</div>
+				</div>
+				<div className="flex flex-col  sm:flex-row justify-between h-[570px]">
+					<section className="w-full sm:w-2/3 sm:border-r-2 border-gray-400 mt-3 pr-2 overflow-auto attendance-scrollbar">
+						<table className="w-full  text-center bg-gray-900">
+							<thead className="sticky top-0 bg-gray-900 z-10">
+								<tr className=" bg-gray-900">
+									<th className="p-1.5 border-r-2 border-gray-500">
+										S no
+									</th>
+									<th className="p-1.5 border-r-2 border-gray-500">
+										Applicant's Name
+									</th>
+									<th className="p-1.5 border-r-2 border-gray-500">
+										Email
+									</th>
+									<th className="p-1.5 border-r-2 border-gray-500">
+										Registered events
+									</th>
+									<th>Status</th>
+								</tr>
+							</thead>
+							<tbody className="text-sm font-mono font-bold]  ">
+								{data?.attendances?.map((applicant, i) => (
+									<tr
+										className={`${
+											i % 2 === 1
+												? "bg-gray-500"
+												: " bg-gray-600"
+										}  `}
+										key={i}
+									>
+										<td>{i + 1}</td>
+										<td className=" p-2">
+											{applicant.userName}
+										</td>
+										<td>{applicant.email}</td>
+										<td className="">
+											{applicant.appliedTo.map(
+												(group) => (
+													<span key={group}>
+														{group + " "}
+													</span>
+												)
+											)}
+										</td>
+										<td
+											className={`${
+												applicant.isAttended
+													? "text-green-500"
+													: "text-red-500"
+											}`}
+										>
+											{applicant.isAttended
+												? "Present"
+												: "Absent"}
+										</td>
+									</tr>
+								))}
+							</tbody>
+						</table>
+					</section>
+					<section className="flex flex-col gap-4 items-center justify-center">
+						<div
+							id="reader"
+							className=" w-[300px] h-[300px] border-2 border-gray-700 "
+						></div>
+
+						{/* <button
+							className="p-2  rounded-md bg-blue-500 hover:bg-blue-300 transition duration-150 text-white hover:text-black"
+							onClick={() =>
+								
+							}
+						>
+							Check Attendance
+						</button> */}
+						<div className=" flex flex-row gap-2 ">
+							<button
+								onClick={startScanner}
+								className="p-2  rounded-md bg-green-500 hover:bg-green-300 transition duration-150 text-white hover:text-black"
+							>
+								Start scanning
+							</button>
+
+							<button
+								onClick={stopScanner}
+								className="p-2  rounded-md bg-red-500 hover:bg-red-300 transition duration-150 text-white hover:text-black"
+							>
+								Stop scanning
+							</button>
+						</div>
+						{scannedUser && (
+							<div className="  self-start border-2 w-full p-3">
+								<h3 className="text-lg font-bold text-green-600 mb-3">
+									Scanner results:
+								</h3>
+								<div className="pl-3 ">
+									<p>Name: {scannedUser?.data.userName}</p>
+									<p>E-mail: {scannedUser?.data.email}</p>
+									<p>
+										Registered to:{" "}
+										{scannedUser?.data.userName}
+									</p>
+								</div>
+							</div>
+						)}
+					</section>
+				</div>
+			</main>
 		</div>
 	);
 };
